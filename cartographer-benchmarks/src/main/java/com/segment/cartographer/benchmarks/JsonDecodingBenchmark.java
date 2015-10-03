a2fdc3fb041f2502b4b8ac54a119b1f5aa7626a0
Prateek Srivastava <f2prateek@gmail.com>
2015-10-03 14:13:41 -0700

    Use fixtures instead of randomly generated data in benchmarks.
    Also improve the run_vogar script and run Vogar on CI.

--- cartographer-benchmarks/src/main/java/com/segment/cartographer/benchmarks/JsonDecodingBenchmark.java
+++ cartographer-benchmarks/src/main/java/com/segment/cartographer/benchmarks/JsonDecodingBenchmark.java
@@ -2,63 +2,59 @@ package com.segment.cartographer.benchmarks;
 
 import com.google.caliper.Param;
 import com.google.caliper.SimpleBenchmark;
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 import com.segment.cartographer.Cartographer;
 import java.io.IOException;
-import java.io.StringReader;
-import java.lang.reflect.Type;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Random;
 import org.json.JSONObject;
 
 public class JsonDecodingBenchmark extends SimpleBenchmark {
 
-  @Param(value = { "10", "50", "100", "1000" }) int length;
-
-  StringReader reader;
+  @Param(value = { "small", "action_label" }) String fixture;
   String json;
+  int blackHole;
 
   @Override protected void setUp() throws Exception {
     super.setUp();
-    Map<String, Object> map = new LinkedHashMap<String, Object>();
 
-    char[] chars = new char[20];
-    Random dice = new Random(0);
-    for (int i = 0; i < chars.length; i++) {
-      chars[i] = (char) dice.nextInt(128);
-    }
-    String data = new String(chars);
-    for (int i = 0; i < length; i++) {
-      map.put(data, data);
-    }
-
-    json = new Cartographer.Builder().build().toJson(map);
-    reader = new StringReader(json);
+    json = jsonFor(fixture);
+    blackHole = 0;
   }
 
-  public void timeCartographer(int reps) throws IOException {
+  static String jsonFor(String name) {
+    if (name.equals("small")) return Json.SMALL;
+    if (name.equals("action_label")) return Json.ACTION_LABEL;
+    throw new IllegalArgumentException("unknown name: " + name);
+  }
+
+  public int timeCartographer(int reps) throws IOException {
     Cartographer cartographer = new Cartographer.Builder().build();
 
     for (int i = 0; i < reps; i++) {
-      cartographer.fromJson(reader);
+      blackHole += cartographer.fromJson(json).size();
     }
+
+    return blackHole;
   }
 
-  public void timeGson(int reps) throws IOException {
+  /*
+  public int timeGson(int reps) throws IOException {
     Gson gson = new Gson();
     Type type = new TypeToken<Map<String, Object>>() {
     }.getType();
 
     for (int i = 0; i < reps; i++) {
-      gson.fromJson(reader, type);
+      Map<String, Object> map = gson.fromJson(json, type);
+      blackHole += map.size();
     }
-  }
 
-  public void timeJSONObject(int reps) throws Exception {
+    return blackHole;
+  }
+  */
+
+  public int timeJSONObject(int reps) throws Exception {
     for (int i = 0; i < reps; i++) {
-      JSONObject jsonObject = new JSONObject(json);
+      blackHole += new JSONObject(json).length();
     }
+
+    return blackHole;
   }
 }
--- run_vogar.sh
+++ run_vogar.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-java -jar vogar.jar --benchmark --verbose --mode device --sourcepath cartographer/src/main/java cartographer-benchmarks/src/main/java/com/segment/cartographer/benchmarks/JsonEncodingBenchmark.java
+SRC=cartographer/src/main/java
+BENCH=cartographer-benchmarks/src/main/java/com/segment/cartographer/benchmarks/Json*Benchmark.java
+
+java -jar vogar.jar --benchmark --mode device --sourcepath $SRC $BENCH
